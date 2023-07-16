39603502e3d5737d74851d39d2a37ad4d94deb4b
WhiskyRodrigo <rodrigo.diocares@inacapmail.cl>
2023-07-16 02:57:00 -0400

    ..

--- Admin/DTO/Lectura.cs
+++ Admin/DTO/Lectura.cs
@@ -10,13 +10,15 @@ namespace Admin.DTO
 {
     public class Lectura
     {
+        private Medidor medidor;
         private DateTime fecha;
         private string consumo;
         private string codigo;
 
-
+        public Medidor Medidor { get => medidor; set => medidor = value; }
         public DateTime Fecha { get => fecha; set => fecha = value; }
         public string Consumo { get => consumo; set => consumo = value; }
         public string Codigo { get => codigo; set => codigo = value; }
+        
     }
 }
